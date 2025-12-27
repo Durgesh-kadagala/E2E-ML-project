7a714d248a1fd4262752dc5fcd0a92260c3f2cf0
Durgesh kadagala <kadagaladurgesh@gmail.com>
2025-12-27 14:48:27 +0530

    dependecies

--- template.py
+++ template.py
@@ -1,1 +1,47 @@
-print("hello world")
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s : ')
+
+project_name = "wine-quality-project"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constant/__init__.py",
+     "config/config.yaml",
+     "params.yaml",
+     "schema.yaml",
+     "main.py",
+     "app.py",
+     "requirements.txt",
+     "setup.py",
+     "research/trials.ipynb",
+     "templates/index.html"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, file_name = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory :  {filedir} for the file : {file_name}")
+        
+    if(not os.path.exists(filepath)) or (os.path.getsize(file_name)==0) :
+        with open(filepath,'w'):
+            pass
+        logging.info(f"Creating empty file :{filepath}")
+
+    else :
+        logging.info(f"{file_name} is already exists")
